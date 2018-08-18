8476a16c361bcb4cf6d68eb4f0500f14eb342778
Abreto Fu <m@abreto.net>
2018-08-19 03:54:26 +0800

    add bzoj 1004

---
+++ vol-01/1004.cc
@@ -0,0 +1,90 @@
+/**
+ * BZOJ 1004
+ * [HNOI2008] Cards
+ * by Abreto<m@abreto.net>.
+ **/
+#include <cstdio>
+#include <bitset>
+#include <cstring>
+
+using namespace std;
+typedef long long int ll;
+
+ll p;
+void add(ll &dest, ll delta) { dest = (dest + delta) % p; }
+void mul(ll &dest, ll times) { dest = (dest * times) % p; }
+ll qow(ll a, ll x)
+{
+    ll ret = 1ll;
+    while (x)
+    {
+        if (1 & x) mul(ret, a);
+        mul(a, a);
+        x >>= 1;
+    }
+    return ret;
+}
+
+#define MAXC (1 << 5)
+#define MAXN (1 << 7)
+
+int n;
+int sr, sb, sg;
+int m;
+int to[MAXN];
+
+bitset<MAXN> vis;
+int ncir;
+int sz[MAXN];
+ll f[MAXC][MAXC][MAXC];
+ll count(void)
+{
+    ncir = 0;
+    memset(sz, 0, sizeof sz); memset(f, 0, sizeof f);
+    vis.reset();
+    for (int i = 1; i <= n; i++)
+    {
+        if (vis.test(i)) continue;
+        int v = i;
+        while (!vis.test(v))
+        {
+            sz[ncir]++;
+            vis.set(v);
+            v = to[v];
+        }
+        ncir++;
+    }
+    // fprintf(stderr, "ncir: %d, sz: %d\n", ncir, sz[0]);
+    f[0][0][0] = 1ll;
+    for (int c = 0; c < ncir; c++)
+    {
+        for (int i = sr; i >= 0; i--)
+            for (int j = sb; j >= 0; j--)
+                for (int k = sg; k >= 0; k--)
+                {
+                    if (i >= sz[c]) add(f[i][j][k], f[i - sz[c]][j][k]);
+                    if (j >= sz[c]) add(f[i][j][k], f[i][j - sz[c]][k]);
+                    if (k >= sz[c]) add(f[i][j][k], f[i][j][k - sz[c]]);
+                }
+    }
+    // fprintf(stderr, "f[%d][%d][%d] = %lld\n", sr, sb, sg, f[sr][sb][sg]);
+    return f[sr][sb][sg];
+}
+
+int main(void)
+{
+    int i, j;
+    ll ans = 0ll;
+    scanf("%d%d%d%d%lld", &sr, &sb, &sg, &m, &p);
+    n = sr + sb + sg;
+    for (i = 0; i < m; i++)
+    {
+        for (j = 1; j <= n; j++)
+            scanf("%d", to + j);
+        add(ans, count());
+    }
+    for (i = 1; i <= n; i++) to[i] = i; add(ans, count());
+    mul(ans, qow(m + 1, p - 2));
+    printf("%lld\n", ans);
+    return 0;
+}
